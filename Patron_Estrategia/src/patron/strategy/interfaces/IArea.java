7595ad106f2b2258b70e44fb8cc4569519b3b2e8
dmendoza01 <115443143+dmendoza01@users.noreply.github.com>
2022-10-09 23:11:18 -0600

    Delete Patron_Estrategia directory

--- Patron_Estrategia/src/patron/strategy/interfaces/IArea.java
+++
@@ -1,7 +0,0 @@
-package patron.strategy.interfaces;
-// Interfaz que especifica el método que implementarán las estrategias.
-public interface IArea {    
-    float calculaArea(); // Calcula el área.
-    String getResultado(); // Devuelve un mensaje con el resultado.
-}
-
